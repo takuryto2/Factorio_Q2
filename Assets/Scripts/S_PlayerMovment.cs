6b4ed8aca5ec96550e097d38335c435cca36ef79
takuryto2 <150046987+takuryto2@users.noreply.github.com>
2024-10-31 15:29:42 +0100

    player movment

--- Assets/Scripts/S_PlayerMovment.cs
+++ Assets/Scripts/S_PlayerMovment.cs
@@ -1,16 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class S_PlayerMovment : MonoBehaviour
 {
     private bool canRotateCam;
     private float yRotation;
+    private float moveDirection;
+    private Rigidbody rb;
     [SerializeField] private float rotationSpeed;
-    public void move(InputAction.CallbackContext context)
-    {
 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float maxMoveSpeed;
+    [SerializeField] private Camera camera;
+    private GameController gameController;
+    private InputAction move;
+    private Vector3 forceDirection;
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameController = new GameController();
+    }
+    private void OnEnable()
+    {
+        move = gameController.GameControls.Movement;
+        gameController.GameControls.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(camera) * moveSpeed;
+        forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(camera) * moveSpeed;
+
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        Vector3 horizontalVelocity = rb.velocity;
+        horizontalVelocity.y = 0f;
+        if (horizontalVelocity.sqrMagnitude > maxMoveSpeed * maxMoveSpeed)
+            rb.velocity = horizontalVelocity.normalized * maxMoveSpeed;
+    }
+
+    private Vector3 GetCameraForward(Camera camera)
+    {
+        Vector3 forward = camera.transform.forward;
+        forward.y = 0;
+        return forward.normalized;
+    }
+
+    private Vector3 GetCameraRight(Camera camera)
+    {
+        Vector3 right = camera.transform.right;
+        right.y = 0;
+        return right.normalized;
     }
 
     public void startRotateCamera(InputAction.CallbackContext context)
