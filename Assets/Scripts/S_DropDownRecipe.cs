e0cdadd1a70ec7e99bed35b8d635b7bfa47c1fc6
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-11-08 16:17:13 +0100

    Dropdown for changing recipe done
    ouga bouga

--- Assets/Scripts/S_DropDownRecipe.cs
+++ Assets/Scripts/S_DropDownRecipe.cs
@@ -8,25 +8,28 @@ using UnityEngine.InputSystem;
 public class S_DropDownRecipe : MonoBehaviour
 {
     TMP_Dropdown dropdown;
+    private S_CrafterBehaviour crafterRef;
 
     private void Start()
     {
         dropdown = GetComponent<TMP_Dropdown>();
         gameObject.SetActive(false);
+        
     }
     public void ChangeDropdownVisibility(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
         {
+            crafterRef = null;
             Ray mouseRay = new();
             mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(mouseRay, out RaycastHit hit))
             {
-                if (hit.collider.TryGetComponent<S_CrafterBehaviour>(out S_CrafterBehaviour crafter))
+                if (hit.collider.TryGetComponent<S_CrafterBehaviour>(out crafterRef))
                 {
                     dropdown.ClearOptions();
-                    AddRecipeToDropdown(crafter);
+                    AddRecipeToDropdown(crafterRef);
                     gameObject.SetActive(!gameObject.activeSelf);
 
                 }
@@ -38,5 +41,10 @@ public class S_DropDownRecipe : MonoBehaviour
         List<SO_Crafts> recipeList = crafter.allRecipe;
         List<string> recipeName = recipeList.ConvertAll<string>(new System.Converter<SO_Crafts, string>((recipe) => recipe.name));   
         dropdown.AddOptions(recipeName);
+    }
+
+    public void GiveRecipe()
+    {
+        crafterRef._recipeSelected=crafterRef.allRecipe[dropdown.value];
     }
 }
